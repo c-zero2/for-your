56f59571c6076eba3684ff72857fa1da34315690
czero <154037290+c-zero2@users.noreply.github.com>
2024-11-11 20:31:18 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("anniversaryForm").addEventListener("submit", function(event) {
-  event.preventDefault();
-  const anniversaryDate = document.getElementById("anniversaryDate").value;
-  if (anniversaryDate) {
-    window.location.href = `love.html?date=${anniversaryDate}`;
-  }
-});
